42d7965e34360dd16d5e41c23144438d72a21bb3
RajaZaryabMehmood <90467516+RajaZaryabMehmood@users.noreply.github.com>
2021-10-03 15:22:15 +0500

    Update dart 4.dart

--- dart 4.dart
+++ dart 4.dart
@@ -1,47 +1,69 @@
 //---Q2
-//  void main(){
-//   var a = 2;
-//    table(a);
-//  }
+ void main(){
+  var a = 2;
+   table(a);
+ }
  
-// void table(int a){
-//   for( var i = 1; i<=10; i++){
-//   print("${a}x${i}=${a*i}");
-//   }
+void table(int a){
+  for( var i = 1; i<=10; i++){
+  print("${a}x${i}=${a*i}");
+  }
  
-// }
+}
 
 //---Q3
-// void main(){
+void main(){
 
  
-//   var a = 2;
-//   number(a);
+  var a = 2;
+  number(a);
 
-// }
+}
 
-// void number(a){
-//   List lst = [2,4,6,8,9];
-//   for(var j in lst){
-//      print(j*a);
-//      }
-// }
+void number(a){
+  List lst = [2,4,6,8,9];
+  for(var j in lst){
+     print(j*a);
+     }
+}
 
 //---Q4
-// void main(){
+void main(){
 
-//   marksheet(); 
+  marksheet(); 
 
-// }
+}
 
-// void marksheet(){
-//   List lst1 = ["raja","zaryab","mehmood","rizwan","zeeshan"];
-//   List lst2 = ["75","85","60","80","90"];
+void marksheet(){
+  List lst1 = ["raja","zaryab","mehmood","rizwan","zeeshan"];
+  List lst2 = ["75","85","60","80","90"];
     
-//       print("${lst1.elementAt(0)}:${lst2.elementAt(0)}");
-//       print("${lst1.elementAt(1)}:${lst2.elementAt(1)}");
-//       print("${lst1.elementAt(2)}:${lst2.elementAt(2)}");
-//       print("${lst1.elementAt(3)}:${lst2.elementAt(3)}");
-//       print("${lst1.elementAt(4)}:${lst2.elementAt(4)}");
+      print("${lst1.elementAt(0)}:${lst2.elementAt(0)}");
+      print("${lst1.elementAt(1)}:${lst2.elementAt(1)}");
+      print("${lst1.elementAt(2)}:${lst2.elementAt(2)}");
+      print("${lst1.elementAt(3)}:${lst2.elementAt(3)}");
+      print("${lst1.elementAt(4)}:${lst2.elementAt(4)}");
         
-// }
+}
+
+//---Q7
+main() {
+  Developer me = Developer(age: 26, name: "Raja Zaryab", education: "BS(CS)");
+  me.sayMyName();
+}
+
+class Developer {
+  //properties definition
+  int age;
+  String name;
+  String education;
+  //constructor
+  Developer({required this.name, required this.age, required this.education});
+
+  //method
+  void sayMyName() {
+    print(" Name      : $name");
+    print(" Age       : $age");
+    print(" Education : $education");
+  }
+}
